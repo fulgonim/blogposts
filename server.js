22755e56abcb94dd01422acb59e1571d74bf0cee
Max Fulgoni <fulgonim@gmail.com>
2018-01-27 11:05:46 -0500

    added runServer and closeServer functions to server.js

--- server.js
+++ server.js
@@ -14,10 +14,40 @@ app.use(morgan('common'));
 //requests for DELETE and PUT that come into /blog-posts/:id are routed to deletePutRouter.js
 
 app.use('/blog-posts', CRUDRouter);
-//app.use('/blog-posts/:id', deletePutRouter);
 
+// declare a server object here for runServer and closeServer functions
+let server;
 
+// this function starts the server and returns a Promise
+function runServer() {
+	const port = process.env.PORT || 8080;
+	return new Promise((resolve, reject) => {
+		server = app.listen(port, () => {
+			console.log(`Your app is listening on ${port}`);
+			resolve(server);
+		}).on('error', err => {
+			reject(err)
+		});
+	});
+}
 
-app.listen(process.env.PORT || 8080, () => {
+function closeServer() {
+	return new Promise((resolve, reject) => {
+		console.log('Closing server');
+		server.close(err => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve();
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+/*app.listen(process.env.PORT || 8080, () => {
 	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+}); */
